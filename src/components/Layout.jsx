ce4b10bf9b4c554bed7c55d4c7bc26c576d87d5f
Odai Mohammed Alkhalaf Alissa <odaia@hiof.no>
2024-04-04 00:03:50 +0200

    commit

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,12 @@
 import { Link, Outlet } from "react-router-dom";
 
-export default function Layout({ children, setSearchTerm }) {
+export default function Layout({ children}) {
 
-    const handleclick = ()=>{
-        setSearchTerm('James Bond')
-    }
+    
     return (
         <div id="container">
             <header>
-                <Link onClick={handleclick} to="/">
+                <Link to="/">
                     <h1 className="bokArkiv">Book Search </h1>
                 </Link>
             </header>
